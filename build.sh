acea257be744f0b7baa7af5084d7970ee1f862a5
Jordan Smith <jsmithnh09@gmail.com>
2026-03-11 22:36:19 -0400

    Adding build entry script. CI hopefully?

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-cmake -S . -B cmake-build
-cd cmake-build
-cmake --build .
-ctest
-exit 0
---
+++ ci/build.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/bash
+#
+# Intended for running (hopefully) in CI??
+
+set -Eeuo pipefail
+
+# groovy one liner.
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+
+# some constants
+SOURCE_DIR=$SCRIPT_DIR/../
+BUILD_DIR=$SCRIPT_DIR/../cmake-build
+GREEN_COLOR='\033[0;32m'
+RESET_COLOR='\033[0m'
+CHECKMARK_ICON='\u2714'
+
+# clean build every time
+rm -rf "$BUILD_DIR"
+
+# check if Cmake installed
+if ! command -v cmake &> /dev/null
+then
+	echo "CMake could not be found."
+	exit 1
+fi
+echo "-- CMake installed."
+
+# make the build directory
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR"
+
+# run generation/configuration.
+echo "-- Generating..."
+cmake -S "$SOURCE_DIR" -B .
+
+# run the build.
+echo "-- Building..."
+cmake --build .
+
+# Run CTest inside the build.
+echo "-- Testing..."
+ctest --output-on-failure
+
+# hurray!
+printf "${GREEN_COLOR}${CHECKMARK_ICON} Success!${RESET_COLOR}\n"
+exit 0
