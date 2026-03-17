c1ece4b373b267abf07847c29cd6b71e69a78b05
Jordan Smith <jsmithnh09@gmail.com>
2026-03-16 21:48:54 -0400

    Adding initial C-wrapper for use in pytest.

--- test/test_coeffs.py
+++ test/test_coeffs.py
@@ -1,3 +1,68 @@
+import pytest
+from ctypes import CDLL, c_size_t, POINTER, c_double
+from pathlib import Path
 import numpy as np
-from scipy import signal
+import sys
+from scipy.signal import butter, cheby1, cheby2, ellip, zpk2sos
 
+# CMake ought to configure this.
+_libenv = os.environ.get("ZSOS_LIB_PATH")
+if not _libenv:
+    raise RuntimeError("ZSOS_LIB_PATH environment variable not set by Cmake.")
+else:
+    _libpath = Path(_libenv)
+    if not _libpath.is_file():
+        raise RuntimeError("Specified library path does not exist.")
+_libstr = str(_libpath.resolve())
+
+
+# putting in the unload function for the DLL.
+def unload_library():
+    if sys.platform == "win32":
+        import kernel32
+
+        close = kernel32.FreeLibrary
+    else:
+        import _ctypes
+
+        close = _ctypes.dlclose
+    close(lib._handle)
+
+
+# Load the library using the path from Cmake/Ctest.
+lib = CDLL(_libstr)
+atexit.register(unload_library)
+
+# exposing the zpk2sos signature for interleaved complex values.
+# EXPORT size_t zpk2sos(const double *, size_t, const double *, size_t, double, double *);
+# EXPORT size_t soscount(size_t, size_t);
+lib.soscount.argtypes = [c_size_t, c_size_t]
+lib.soscount.restype = c_size_t
+lib.zpk2sos.argtypes = [
+    POINTER(c_double),
+    c_size_t,
+    POINTER(c_double),
+    c_size_t,
+    c_double,
+    POINTER(c_double),
+]
+lib.zpk2sos.restype = c_size_t
+
+
+def zpk2sos_wrapper(z, p, k):
+    # need to enforce complex values.
+    z = np.complex128(z)
+    p = np.complex128(p)
+
+    z_ptr = z.ctypes.data_as(POINTER(c_double))
+    p_ptr = p.ctypes.data_as(POINTER(c_double))
+    kc = c_double(k)
+
+    numz = c_size_t(len(z))
+    nump = c_size_t(len(p))
+    nstages = lib.soscount(nz, np)
+    sosmat = np.zeros((nstages, 6), dtype=np.float64)
+
+    sos_ptr = sosmat.ctypes.data_as(POINTER(c_double))
+    err = lib.zpk2sos(z_ptr, numz, p_ptr, nump, kc, sos_ptr)
+    return (sosmat, err)
