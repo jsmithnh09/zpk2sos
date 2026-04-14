6b77fcb2a2068cbb9beca8eb48ec0d225fcd904f
Jordan Smith <jsmithnh09@gmail.com>
2026-04-14 01:35:39 -0400

    Updating CI script to use presets.

--- ci/build.sh
+++ ci/build.sh
@@ -1,46 +1,41 @@
 #!/usr/bin/bash
 #
-# Intended for running (hopefully) in CI??
+# Intended for running in CI
 
 set -Eeuo pipefail
 
 # groovy one liner.
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 
-# some constants
-SOURCE_DIR=$SCRIPT_DIR/../
-BUILD_DIR=$SCRIPT_DIR/../cmake-build
+cd "$SCRIPT_DIR/.."
 GREEN_COLOR='\033[0;32m'
 RESET_COLOR='\033[0m'
-CHECKMARK_ICON='\u2714'
+CHECKMARK='\u2714'
 
-# clean build every time
-rm -rf "$BUILD_DIR"
-
-# check if Cmake installed
 if ! command -v cmake &> /dev/null
 then
 	echo "CMake could not be found."
 	exit 1
 fi
 echo "-- CMake installed."
+if ! command -v uv &> /dev/null
+then
+	echo "uv python manager not found."
+	exit 1
+fi
+echo "-- uv installed."
 
-# make the build directory
-mkdir -p "$BUILD_DIR"
-cd "$BUILD_DIR"
+# generate the virtual environment for testing.
+uv venv --clear
+uv pip install -e .
 
-# run generation/configuration.
-echo "-- Generating..."
-cmake -S "$SOURCE_DIR" -B .
+# Build the files.
+uv run cmake --preset test
+uv run cmake --build --preset test
 
-# run the build.
-echo "-- Building..."
-cmake --build .
-
-# Run CTest inside the build.
-echo "-- Testing..."
-ctest -V --output-on-failure
+# Running CTest.
+uv run ctest --preset test -V --output-on-failure
 
 # hurray!
-printf "${GREEN_COLOR}${CHECKMARK_ICON} Success!${RESET_COLOR}\n"
+printf "${GREEN_COLOR}${CHECKMARK} Success!${RESET_COLOR}\n"
 exit 0
